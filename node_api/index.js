d85f067928a3cec337ade4e25085146f07701ded
Slovo Ulo <slovojoe33@gmail.com>
2023-09-22 00:14:11 +0300

    Heroku PORT config

--- node_api/index.js
+++ node_api/index.js
@@ -8,7 +8,7 @@ app.use(express.json());
 
 app.use('/gamesapi', gameRoutes);
 
-const port = process.env.port || 3000;
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
